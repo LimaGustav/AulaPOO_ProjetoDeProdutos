5e39babd7e5dd811a07a697cf2e3c98bd054cfbb
LimaGustav <lima.gustavo100@homtail.com>
2021-06-07 19:48:25 -0300

    Métodos (menos construtuor)

--- Classes/Login.cs
+++ Classes/Login.cs
@@ -1,22 +1,34 @@
+using System;
 using ProjetoProduto.Interfaces;
 
 namespace ProjetoProduto.Classes
 {
     public class Login : ILogin
     {
+        public bool Logado { get; set; }
+        
         public string Deslogar(Usuario usuario)
         {
-            throw new System.NotImplementedException();
+            if (Logado)
+            {
+                Logado = false;
+                return ($"{usuario.Nome} saiu");
+            }
+            return ($"{usuario.Nome} já está deslogado")
         }
 
         public string Logar(Usuario usuario)
         {
-            throw new System.NotImplementedException();
+            if (!Logado) {
+                Logado = true;
+                return ($"{usuario.Nome} entrou");
+            }
+            return ($"{usuario.Nome} já está logado");
         }
 
         void ILogin.Login()
         {
-            throw new System.NotImplementedException();
+            Console.WriteLine("Teste");
         }
     }
 }
