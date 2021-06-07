7b443be760ed19d5f86bc92e223267c894b46dbe
LimaGustav <lima.gustavo100@homtail.com>
2021-06-07 19:11:45 -0300

    Produto (Interface, Classe)

---
+++ Interfaces/IProduto.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using ProjetoProduto.Classes;
+
+namespace ProjetoProduto.Interfaces
+{
+    public interface IProduto
+    {
+         string Cadastrar (Produto produto);
+        List<Produto> Listar();
+        string Deletar(Produto produto);
+    }
+}
