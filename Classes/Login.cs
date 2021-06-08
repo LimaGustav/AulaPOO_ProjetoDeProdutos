49f1dd0a7dccfa5133541f95c2db32acf1016fcc
LimaGustav <lima.gustavo100@homtail.com>
2021-06-08 15:47:43 -0300

    Cadastrar usuario

--- Classes/Login.cs
+++ Classes/Login.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading;
 using ProjetoProduto.Interfaces;
 
 namespace ProjetoProduto.Classes
@@ -28,52 +29,70 @@ namespace ProjetoProduto.Classes
 
         public Login()
         {
+            Usuario usuarioPai = new Usuario();
             bool isInt;
             int opcInt;
+            int codUser = 0;
 
             do
             {
-                Console.Clear();
-                Console.WriteLine("O que você deseja fazer? ");
-                Console.WriteLine("Cadastrar usuário [1]\nDeletar usuário [2]");
-                Console.Write("Logar [3]\n Deslogar [4]\n-> ");
-                string opcString = Console.ReadLine();
-                isInt = int.TryParse(opcString, out opcInt);
-            } while (!isInt);
+                do
+                {
+                    Console.Clear();
+                    Console.WriteLine("O que você deseja fazer? ");
+                    Console.WriteLine("Cadastrar usuário [1]\nDeletar usuário [2]");
+                    Console.Write("Logar [3]\nDeslogar [4]\n-> ");
+                    string opcString = Console.ReadLine();
+                    isInt = int.TryParse(opcString, out opcInt);
+                } while (!isInt);
 
-            switch (opcInt)
-            {
-                case 1:
-                    Console.WriteLine("Qual seu nome? ");
-                    string nomeUser = Console.ReadLine().ToUpper();
+                switch (opcInt)
+                {
+                    case 1: // Cadastrar usuário
+                        Console.WriteLine("Qual seu nome? ");
+                        string nomeUser = Console.ReadLine().ToUpper();
 
-                    Console.WriteLine("Digite seu Email");
-                    string emailUser = Console.ReadLine().ToLower();
+                        Console.WriteLine("Digite seu Email");
+                        string emailUser = Console.ReadLine().ToLower();
 
-                    bool senhaCorreta;
+                        bool senhaCorreta;
 
-                    do
-                    {
-                        Console.WriteLine("Cadastre uma senha: ");
-                        string senhaUser = Console.ReadLine();
+                        do
+                        {
+                            Console.WriteLine("Cadastre uma senha: ");
+                            string senhaUser = Console.ReadLine();
 
-                        Console.WriteLine("Digite novamente a senha: ");
-                        string confirmaSenha = Console.ReadLine();
-                        if (senhaUser == confirmaSenha) {
-                            senhaCorreta = true;
-                            Usuario usuario1 = new Usuario();
-                        } else {
-                            senhaCorreta = false;
-                            Console.Clear();
-                            Console.ForegroundColor = ConsoleColor.Red;
-                            Console.WriteLine("Senhas divergentes.");
-                            Console.ResetColor();
-                        }
-                    } while (!senhaCorreta);
-                    break;
-                default:
-                    break;
-            }
+                            Console.WriteLine("Digite novamente a senha: ");
+                            string confirmaSenha = Console.ReadLine();
+                            if (senhaUser == confirmaSenha) {
+                                senhaCorreta = true;
+                                codUser += 1;
+                                Console.ForegroundColor = ConsoleColor.Green;
+                                Console.WriteLine();
+                                Console.WriteLine(usuarioPai.Cadastrar(new Usuario(codUser,nomeUser,emailUser,senhaUser)));
+                                Thread.Sleep(1000);
+                                Console.ResetColor();
+                                
+                            } else {
+                                senhaCorreta = false;
+                                Console.Clear();
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.WriteLine("Senhas divergentes.");
+                                Console.ResetColor();
+                            }
+                        } while (!senhaCorreta);
+                        break;
+
+                        case 2: // Deletar usuario
+
+                            break;
+
+
+                    default:
+
+                        break;
+                }
+            } while (opcInt != 0);
             
         }
     }
